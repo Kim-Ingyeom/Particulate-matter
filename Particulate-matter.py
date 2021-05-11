ee09e366dfd2197df9d3639d61ee64761ea8b3a9
Kim-Ingyeom <want2704@gmail.com>
2021-05-11 12:58:48 +0900

    '수정'

--- Particulate-matter.py
+++ Particulate-matter.py
@@ -7,11 +7,27 @@ soup = BeautifulSoup(page.content,'html.parser')
 
 table = soup.find_all('td')
 ap = []
+pm = []#미세먼지
+upm = []#초 미세먼지
+co = []#일산화 탄소
+o3 = []#오존
 for i in table[2:]:
     ap.append(i.text)
 print(ap)
-
-
+for i in ap:
+    if ap.index(i)%7 == 1:
+        pm.append(i)
+    if ap.index(i)%7 == 2:
+        upm.append(i)
+    if ap.index(i)%7 == 4:
+        co.append(i)
+    if ap.index(i)%7 == 6:
+        o3.append(i)
+print('당진, 일산, 동해바이오, 울산, 호남')
+print(pm)
+print(upm)
+print(co)
+print(o3)
 
 
 
